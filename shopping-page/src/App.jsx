29477b1c5bf0d7a76241348ab1d2a2f99e79fc5e
Tej Mishra <jetmi@live.com>
2025-02-07 20:26:26 -0800

    Fixed npm run issue

---
+++ shopping-page/src/App.jsx
@@ -0,0 +1,12 @@
+import cart from "./cart.svg"
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div><button ><img src={cart} alt="cart" /></button></div>
+  )
+}
+
+export default App
--- vite-project/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import './App.css'
-
-function App() {
-
-  return (<div><h1>This is the bruh page for the bruh people</h1></div>)
-}
-
-export default App
